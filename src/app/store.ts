b0f02f8d404bacdd378becf53b051f27b4838d6e
motokikando <kando813motoki@gmail.com>
2021-09-27 22:43:08 +0900

    code整理

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,6 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import authReducer from "../features/auth/authSlice"
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import authReducer from "../features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
@@ -9,13 +9,7 @@ export const store = configureStore({
   },
 });
 
-
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
 
 export type AppDispatch = typeof store.dispatch;
