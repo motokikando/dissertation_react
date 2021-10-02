84bf895841c5b753c4401ea057f64e530ef4d975
motokikando <kando813motoki@gmail.com>
2021-10-02 23:27:54 +0900

    メイン画面のUI作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import styles from "./App.module.css";
 import { Grid, Avatar } from "@material-ui/core";
-import { makeStyles, MuiThemeProvider, Theme } from "@material-ui/core/styles";
+import { makeStyles, MuiThemeProvider, Theme, createMuiTheme } from "@material-ui/core/styles";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import PolymerIcon from "@material-ui/icons/Polymer";
 import { useSelector, useDispatch } from "react-redux";
@@ -24,22 +24,114 @@ import ThesisForm from "./features/thesis/ThesisForm";
 
 import { AppDispatch } from "./app/store";
 
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: "#3cb371",
+    },
+  },
+});
 
+const useStyles = makeStyles((theme: Theme) => ({
+  icon: {
+    marginTop: theme.spacing(3),
+    cursor: "none",
+  },
+  avatar: {
+    marginLeft: theme.spacing(1),
+  },
+}));
 
-const App = () => {
+const App: React.FC = () => {
   const classes = useStyles();
   const dispatch: AppDispatch = useDispatch();
   const editedThesis = useSelector(selectEditedThesis);
   // const theses = useSelector(selectTheses);
   const loginUser = useSelector(selectLoginUser);
   const profiles = useSelector(selectProfiles);
+  //ログインユーザーに対応するプロフィールのオブジェクトをfilterを使って取得
+  const loginProfile = profiles.filter((prof) => prof.user_profile === loginUser.id)[0];
 
-  const logiinProfile = profiles.filter(
-    
-  )
+  //ログアウト JWTのトークンを削除する関数
+  const Logout = () => {
+    localStorage.removeItem("localJWT");
+    //ログイン画面に遷移
+    window.location.href = "/Auth";
+  };
 
+  //imageInputをクリックすることでファイルダイアログを開く
+  const handlerEditPicture = () => {
+    const fileInput = document.getElementById("imageInput");
+    fileInput?.click();
+  };
 
-  return <div className="App">App</div>;
+  useEffect(() => {
+    //Appcomponentからレンダリングされた時にAPIからまとめて呼び出す
+    const fetchBootLoader = async () => {
+      await dispatch(fetchAsyncGetTheses());
+      await dispatch(fetchAsyncGetMyProf());
+      await dispatch(fetchAsyncGetUsers());
+      await dispatch(fetchAsyncGetCategory());
+      await dispatch(fetchAsyncGetProfs());
+    };
+    fetchBootLoader();
+  }, [dispatch]);
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <div className={styles.app__root}>
+        <Grid container>
+          <Grid item xs={4}>
+            <PolymerIcon className={classes.icon}></PolymerIcon>
+          </Grid>
+          <Grid item xs={4}>
+            <h1>Dissertation List Board</h1>
+          </Grid>
+          <Grid item xs={4}>
+            <div className="styles app__logout">
+              <button className="styles app__iconLogout" onClick={Logout}>
+                <ExitToAppIcon fontSize="large" />
+              </button>
+              <input
+                type="file"
+                id="imageInput"
+                hidden={true}
+                onChange={(e) => {
+                  dispatch(
+                    fetchAsyncUpdateProf({
+                      id: loginProfile.id,
+                      img: e.target.files !== null ? e.target.files[0] : null,
+                    })
+                  );
+                }}
+              />
+              <button className={styles.app__btn} onClick={handlerEditPicture}>
+                <Avatar
+                  className={classes.avatar}
+                  alt="avatar"
+                  src={loginProfile?.img !== null ? loginProfile?.img : undefined}
+                />
+              </button>
+            </div>
+          </Grid>
+          <Grid item xs={8}>
+            <ThesisList />
+          </Grid>
+          <Grid item xs={4}>
+            <Grid
+              container
+              direction="column"
+              alignItems="center"
+              justify="center"
+              style={{ minHeight: "80vh" }}
+            >
+              <Grid item>{editedThesis.category ? <ThesisForm /> : <ThesisDisplay />}</Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    </MuiThemeProvider>
+  );
 };
 
 export default App;
