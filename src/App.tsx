b82b63edce790cc81e982b881415c7ef06cb2816
motokikando <kando813motoki@gmail.com>
2021-10-02 10:49:54 +0900

    メイン画面の構築

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect } from "react";
+import styles from "./App.module.css";
+import { Grid, Avatar } from "@material-ui/core";
+import { makeStyles, MuiThemeProvider, Theme } from "@material-ui/core/styles";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import PolymerIcon from "@material-ui/icons/Polymer";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectLoginUser,
+  selectProfiles,
+  fetchAsyncGetMyProf,
+  fetchAsyncGetProfs,
+  fetchAsyncUpdateProf,
+} from "./features/auth/authSlice";
+import {
+  fetchAsyncGetTheses,
+  fetchAsyncGetUsers,
+  fetchAsyncGetCategory,
+  selectEditedThesis,
+} from "./features/thesis/thesisSlice";
+import ThesisList from "./features/thesis/ThesisList";
+import ThesisDisplay from "./features/thesis/ThesisDisplay";
+import ThesisForm from "./features/thesis/ThesisForm";
 
-function App() {
+import { AppDispatch } from "./app/store";
+
+
+
+const App = () => {
+  const classes = useStyles();
+  const dispatch: AppDispatch = useDispatch();
+  const editedThesis = useSelector(selectEditedThesis);
+  // const theses = useSelector(selectTheses);
+  const loginUser = useSelector(selectLoginUser);
+  const profiles = useSelector(selectProfiles);
+
+  const logiinProfile = profiles.filter(
+    
+  )
+
+
   return <div className="App">App</div>;
-}
+};
 
 export default App;
---
+++ src/features/thesis/ThesisList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ThesisList: React.FC = () => {
+  return <div></div>;
+};
+
+export default ThesisList;
